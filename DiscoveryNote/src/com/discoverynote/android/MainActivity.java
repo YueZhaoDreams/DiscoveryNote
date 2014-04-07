a2f79ed73bbeee5394449cc32b2db2f4b3cb73bf
LittleFire1989 <dancedfire@163.com>
2014-04-06 20:41:05 -0700

    Set up layout

--- DiscoveryNote/src/com/discoverynote/android/MainActivity.java
+++ DiscoveryNote/src/com/discoverynote/android/MainActivity.java
@@ -1,15 +1,20 @@
-package com.discoverynote;
+package com.discoverynote.android;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	private Button mStartButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mStartButton = (Button)findViewById(R.id.start_button);
 	}
 
 	@Override
